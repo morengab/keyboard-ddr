b6e8fe6e70374726c46359131184e3ec25283c5b
wongas <Anthony@X-Wing.local>
2013-09-14 18:03:11 -0400

    getting shortcuts from DB updates

--- getShortcuts.php
+++ getShortcuts.php
@@ -1,47 +1,36 @@
 <?php
-/*
-if (isset($_POST['url'])) {
-	echo file_get_contents("http://".SanitizeString($_POST['url']));
-	
-	
-}
-
-function SanitizeString($var) {
-	$var = strip_tags($var);
-	$var = htmlentities($var);
-	return stripslashes($var);
-}
-*/
-
 	require_once 'downloads/medoo.min.php';
  
  	$database = new medoo('macadamia_cluster');
  
- 	if (isset($_GET['appID'])) {
+ 	if (isset($_GET['app_name'])) {
 		if (isset($_GET['name'])) {
 			// Get a shortcut by name and app
 			$results = $database->select("shortcuts",
 											"*",
 											[
-												"app_id" => $_GET['appID'],
-												"name" => $_GET['name']
+												"AND" => 
+													[
+														"app_name" => $_GET['app_name'],
+														"name" => $_GET['name']
+													]
 											]);
 								
 			echo json_encode($results);
 		} else {
-			// Get all shortcuts for an app
+			// Get all shortcuts for one app
 			$results = $database->select("shortcuts",
 											"*",
 											[
-												"app_id" => $_GET['appID']
+												"app_name" => $_GET['app_name']
 											]);
 			
 			echo json_encode($results);
 		}		
  	} else {
+		// Get all shortcuts for all apps
 		$results = $database->select("shortcuts", "*");
  
  		echo json_encode($results);
-	}
-						
+	}						
 ?>
