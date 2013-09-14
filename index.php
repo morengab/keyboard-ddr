56915210f8e105ccb46762153c6843dae8bc802d
Matthew Hsieh <mhsieh820@gmail.com>
2013-09-14 13:40:25 -0400

    add initial framework

--- index.php
+++ index.php
@@ -12,33 +12,33 @@
 	<meta name="description" content="">
 	<meta name="keywords" content="" />
 	<meta name="author" content="">
-	<meta name="viewport" content="width=device-width; initial-scale=1.0">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<!-- !CSS -->
 	<link rel="stylesheet/less" text="text/css" href="css/normalize.less">
 	<link rel="stylesheet/less" text="text/css" href="css/base.less">
 	<!-- Uncomment if you are specifically targeting less enabled mobile browsers
 	<link rel="stylesheet" media="handheld" href="css/handheld.css?v=1">  -->
 	<!-- !Modernizr - All other JS at bottom -->
+	<!--[if lt IE 9]>
+			<script src="//html5shim.googlecode.com/svn/trunk/html5.js"></script>
+		<![endif]-->
 	<script src="js/less.js" type="text/javascript"></script>
 </head>
 <!-- !Body -->
 <body>
-	<div id="container">
-		<header>
-			<ul id="nav">
-				<li><a href="#nowhere" title="Home">Home</a></li>
-			</ul><!-- /#nav  -->
-		</header><!-- /header -->		
-		<section id="main">		
+	<div id="container">	
+		<section class="container" id="main">		
+				<div class="four columns alpha" id="left-col">A</div>
+				<div class="twelve columns omega" id="main-col">B</div>
 		</section><!-- /main -->		
 		<footer>
 		</footer><!-- /footer -->
 	</div><!--!/#container -->
 	<!-- !Javascript - at the bottom for fast page loading -->
 	<!-- Grab Google CDN's jQuery. fall back to local if necessary -->
-	<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.4.2/jquery.min.js"></script>
-	<script>!window.jQuery && document.write('<script src="js/jquery-1.4.2.min.js"><\/script>')</script>
-	<script src="js/plugins.js?v=1"></script>
-	<script src="js/script.js?v=1"></script>
+	<script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+	<script>!window.jQuery && document.write('<script src=//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"><\/script>')</script>
+	<script src="js/hotkeys.js"></script>
+	<script src="js/game.js"></script>
 </body>
 </html>
---
+++ js/game.js
@@ -0,0 +1,15 @@
+$j = jQuery.noConflict();
+
+$j(document).ready(function () {
+
+
+
+
+
+
+
+
+});
+
+//create game object
+
