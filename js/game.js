3d29cc1373b66b27a7a75e87202fabb650a05da0
Matthew Hsieh <mhsieh820@gmail.com>
2013-09-14 13:47:14 -0400

    js comments

--- js/game.js
+++ js/game.js
@@ -2,14 +2,21 @@ $j = jQuery.noConflict();
 
 $j(document).ready(function () {
 
+	//create game objects
+	
+	
+	//create keyboard mapping
+	
+	
+	//assign icons
+	
+	
+	//set score
 
 
-
-
+	//play audio
 
 
 
 });
-
-//create game object
 
