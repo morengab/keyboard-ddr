0b494a038002e0a8cd2fab8e59f6b97477cfa0ed
gaby <morengab@gmail.com>
2013-09-15 01:03:32 -0400

    sel

--- test.php
+++ test.php
@@ -65,47 +65,27 @@ $database = new medoo('macadamia_cluster');
 							
 							foreach ($results as $result) :
 							?>
-							<div class="icon_selector" id="icon-<?php echo $result['id']; ?>" data-id="<?php echo $result['id']; ?>" style="background:url('<?php echo $result['image']; ?>') top left no-repeat transparent; background-size: 75px 75px;">	
+							<div 
+								class="icon_selector" 
+								id="icon-<?php echo $result['id']; ?>" 
+								data-id="<?php echo $result['id']; ?>" 
+								style="background:url('<?php echo $result['image']; ?>') top left no-repeat transparent; background-size: 75px 75px;">	
 							</div>
 							
 							<?php
 							endforeach;
-							
 							?>
-						<!--
-
-							<div class="icon_selector">	
-							</div>
-							<div class="icon_selector">	
-							</div>
-							<div class="icon_selector">	
-							</div>
-							<div class="icon_selector">	
-							</div>
-							<div class="icon_selector">	
-							</div>
-							<div class="icon_selector">	
-							</div>
-							<div class="icon_selector">	
-							</div>
-							<div class="icon_selector">	
-							</div>
-							<div class="icon_selector">	
-							</div>
-							<div class="icon_selector">	
-							</div>
-							<div class="icon_selector">	
-							</div>
-							<div class="icon_selector">	
-							</div>
-							<div class="icon_selector">	
-							</div>
-							<div class="icon_selector">	
-							</div>
-							<div class="icon_selector">	
-							</div>
--->
 						</div>
+						
+
+						<ul class="active-selections">
+							Currently selected:
+							<li>app_name: hot key</li>
+							<li>app_name: hot key</li>
+							<li>app_name: hot key</li>
+							<li>app_name: hot key</li>
+						</ul>
+
 						<button class="button" id="start">Start</button>
 					</div>
 					
